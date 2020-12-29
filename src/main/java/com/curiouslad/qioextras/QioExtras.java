41ff464b85975381375269ca927833e131cb7adb
Yuvrajsb <68624376+Yuvrajsb@users.noreply.github.com>
2020-12-29 10:22:55 +0530

    First recipes

--- src/main/java/com/curiouslad/qioextras/QioExtras.java
+++ src/main/java/com/curiouslad/qioextras/QioExtras.java
@@ -24,7 +24,7 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("qioextras")
+@Mod(QioExtras.MOD_ID)
 
 
 public class QioExtras
